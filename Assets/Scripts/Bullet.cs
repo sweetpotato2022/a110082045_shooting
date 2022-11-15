bdd582062174495681e3741c53cccce60175bd2d
Sweetpotato2022 <butterhank@gmail.com>
2022-11-15 22:50:45 +0800

    level 1 子彈不穿牆 停頓射擊 迷宮 碰黃球進下一關 多幾隻怪 level 2 類電流急急棒 碰地重頭開始 碰黃球回到第一關

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 30f;
+    public float atk = 50f;
+    private Rigidbody rb;
+    private float lifeTime = 0;
+    void Start()
+    {
+        // 往前飛
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    void Update()
+    {
+        lifeTime += Time.deltaTime;
+        if (lifeTime > 3)
+        {
+            gameObject.gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 如果碰撞到的是子彈
+        if (other.tag == "Enemy")
+        {
+            // 刪除自己
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        if (other.tag == "Wall")
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
